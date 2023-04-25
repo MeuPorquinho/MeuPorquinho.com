7f910f4fd2037005c67a922ec2a7ad53dc38da4f
marcofeliponi <marco.feliponi@outlook.com>
2023-04-25 20:01:31 -0300

    feat: server express routes

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-require('dotenv').config();
+require('dotenv').config({ path: './environments/.env' });
+
+const routes = require('./routers/routes');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -13,8 +15,8 @@ connection.once('open', () => {
     console.log("MongoDB database connection established successfully");
 })
 
-app.use(cors());
 app.use(express.json());
+app.use(cors(), routes);
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
