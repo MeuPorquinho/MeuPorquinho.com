7aa65bddcfc41b33ad94b8a9fc6467c4ab54d1f6
marcofeliponi <marco.feliponi@outlook.com>
2023-10-27 19:35:08 -0300

    feat: update password and email routes

--- backend/routers/userRouter/userRouter.js
+++ backend/routers/userRouter/userRouter.js
@@ -6,6 +6,8 @@ const router = express.Router();
 router.post('/login', userService.login)
 router.post('/register', userService.register)
 router.put('/financial-manager', userService.saveFinances)
+router.put('/password', userService.changePassword)
+router.put('/email', userService.changeEmail)
 router.get('/', userService.getUsers);
 router.delete('/:username', userService.deleteUser);
 
