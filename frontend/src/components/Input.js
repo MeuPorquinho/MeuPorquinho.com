71bdfda6cd28cab9e3d957e960471fff4b184ee6
marcofeliponi <marco.feliponi@outlook.com>
2023-11-30 21:32:37 -0300

    feat: new props to dynamic input

--- frontend/src/components/Input.js
+++ frontend/src/components/Input.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Input = ({ label, placeholder, value, onChange, width, color }) => {
+const Input = ({ label, placeholder, value, onChange, width, color, type = 'text', autoComplete = 'on' }) => {
     return (
         <div className={`mb-4 w-${width ? width : '2/4'}`}>
             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor={label}>
@@ -9,8 +9,9 @@ const Input = ({ label, placeholder, value, onChange, width, color }) => {
             <input
                 className={`w-full p-2 rounded text-${color ? color : 'gray-700'} shadow appearance-none leading-tight focus:outline-none focus:shadow-outline`}
                 id={label}
-                type="text"
+                type={type}
                 placeholder={placeholder}
+                autoComplete={autoComplete}
                 value={value}
                 onChange={onChange}
             />
